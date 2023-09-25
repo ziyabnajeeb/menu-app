caf11ac57d2d11d53e7dd38e52f40de524b44fa5
ziyabnajeeb <40266679+ziyabnajeeb@users.noreply.github.com>
2023-09-25 17:29:23 +0500

    data added

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -4,7 +4,7 @@ export const Main = styled.main(({ hasBackground }) => [
   tw`flex h-screen w-screen flex-col px-6`,
   hasBackground ? tw`bg-gradient-to-br from-slate-200 to-slate-50` : tw`bg-slate-200`,
 ]);
-export const Section = tw.section`container`;
+export const Section = tw.section`container mx-auto`;
 export const SectionHeading = tw.div``;
 export const SectionHeadingText = css`
   ${tw`mb-2 px-4 text-center font-extrabold text-4xl text-slate-600 after:(mx-auto block w-32 border-amber-400 border-b-8 pt-5 text-center content-[''])`}
